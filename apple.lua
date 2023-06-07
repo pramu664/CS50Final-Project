09da17ae6418e6c8c4c18b20230b64903e309dba
pramu664 <pramudithaayeshmantha0@gmail.com>
2023-06-07 21:19:13 +0530

    Add different types of tiles .

--- apple.lua
+++ apple.lua
@@ -4,7 +4,7 @@ function Apple:new()
     self.image = love.graphics.newImage("assets/world/apple.png")
     self.x = love.math.random(100, 600)
     self.y = love.math.random(100, 300)
-    self.size = 3
+    self.size = 2.5
     self.width = self.image:getWidth() * self.size
     self.height =  self.image:getHeight() * self.size
 end
